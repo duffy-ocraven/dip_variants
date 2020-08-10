0fc131539692d600d8286df8a2af066cb153d7b1
Vincent B <32371970+V-Born@users.noreply.github.com>
2020-08-10 13:14:50 -0400

    Updating loadDiplomacy.py
    Added parameters, bug fixes

--- main/loadDiplomacyGame.py
+++ main/loadDiplomacyGame.py
@@ -9,33 +9,48 @@ import sys, getopt, os
 
 # Creating a game
 # map file's baseName can be the map_name argument. e.g. Game(map_name='pure')
-
 # the following script, from https://pypi.org/project/diplomacy/
 # plays a game locally by submitting random valid orders until the game is completed.
 def main():
   try:   
    (options, arguments) = getopt.getopt(sys.argv[1:], 'h')
   except getopt.error:
-   sys.exit("Unknown input parameter.")
+
   if [] == arguments:
-    arguments = ["pure"]
-  if not os.path.exists(arguments[0] + ".map"):
-    sys.exit("%s.map could not be opened" % (arguments[0],))
+    sys.exit("Unknown input parameter.")
+
+  file_name = arguments[0]
+  phrase = arguments[1]
+  with open(file_name, 'r') as file:
+    for line in file:
+      input_combined_lines = ''
+      if(line.strip().find("orders") != -1):
+        input_combined_lines += '"orders":{},"results":{},"messages":[]}]}'
+        input = json.loads(input_combined_lines)
+        break
+      input_combined_lines += line.strip()
+    else:
+      sys.exit("File %s is invalid")
+
   input_path = "../unitTestPureGame.json"
   if not os.path.exists(input_path):
     sys.exit("File %s does not exist." % input_path)
   with open(input_path, 'r') as file:
-    combinedLines = ''
+    combined_lines = ''
     for line in file:
-      combinedLines += line.strip()
+      game_combined_lines += line.strip()
       if(line.strip()== ''):
-        saved_game = json.loads(combinedLines)
+        saved_game = json.loads(game_combined_lines)
         game = from_saved_game_format(saved_game)
-	break
+        break
+    else:
+      sys.exit("File %s is invalid")
 
   if not is_valid_saved_game(saved_game):
     sys.exit("File %s was evaluated as invalid." % input_path)
   if not game.is_game_done:
+    # For each power, the F1922M orders are already set
+    game.process() # process those, then for W1922A, do random
     # Getting the list of possible orders for all locations
     possible_orders = game.get_all_possible_orders()
 
@@ -47,11 +62,12 @@ def main():
             loc = loc[:-1]
           if possible_orders[loc]:
             power_orders.append(random.choice(possible_orders[loc]))
+            print("%s for %s" % (power_orders[-1], power_name))
         game.set_orders(power_name, power_orders)
     game.process()
 # Exporting the game to disk to visualize
     with open('unitTestResult_game.json', 'w') as outp:
       outp.write(to_saved_game_format(game))
-      
+
 if __name__ == '__main__':
    main()
